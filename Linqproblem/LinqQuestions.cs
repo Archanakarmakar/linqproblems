ba96a05b968445254a399cb0509431a798a63484
Sumit Karmakar <archanakmr83@gmail.com>
2019-12-18 11:54:16 -0600

    A string of letter terriell and returns an alphabetically order

--- Linqproblem/LinqQuestions.cs
+++ Linqproblem/LinqQuestions.cs
@@ -12,11 +12,27 @@ namespace Linqproblem
         {
             List<string> thList = list.Where(t => t.Contains("th")).ToList();
             return thList;
-                   }
+        }
         public List<string> GetStringNotDuplicate(List<string> list)
         {
             List<string> dupList = list.Distinct().ToList();
             return dupList;
         }
+        public void GetStringOfLetters()
+        {
+            string input = Console.ReadLine();
+            var result1 = input.ToUpper().ToArray().OrderBy(x => x);        //To Arry, OrderBy
+            var result2 = result1.GroupBy(x => x).Select(x => $"{x.Last()}{x.Count()}");                //GroupBy and Count
+            string result = "";
+            foreach (var item in result2)
+            {
+                result += item.ToString();
+            }
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
     }
+
 }
