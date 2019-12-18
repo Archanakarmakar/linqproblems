92a3849d86798d8f99df51d10cf9755ce6e1950c
Chong <chong_1988@hotmail.com>
2019-12-18 10:57:45 -0600

    pulling

--- Linqproblem/Program.cs
+++ Linqproblem/Program.cs
@@ -8,8 +8,21 @@ namespace Linqproblem
 {
     class Program
     {
+        public delegate string GetthWords(string s);
         static void Main(string[] args)
         {
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            List<string> thwords;  
+            thwords = words.Where(w => w.Contains("th")).ToList();
+            for (int i = 0; i < thwords.Count; i++)
+            {
+
+                Console.WriteLine(thwords[i]);
+            }
+
+            Console.ReadLine();
         }
+
+     
     }
 }
